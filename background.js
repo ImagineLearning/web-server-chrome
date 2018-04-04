deea91cfc1594163fd3d656e9c86889afa4a612c
lincolncoe <lincolncoe@gmail.com>
2018-04-04 15:45:01 -0600

    Only allow 1 app instance open

--- background.js
+++ background.js
@@ -241,6 +241,9 @@ function launch(launchData) {
 		var height = screenHeight;
 
 		chrome.app.window.create(page, {
+			//We only want to allow a single instance of our app open at a time
+			id: "", 
+			singleton: true,
 			outerBounds: {
 				width: width,
 				height: height
